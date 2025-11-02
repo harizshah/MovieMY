e6c99680a3accc752641baf040d87b2903c11aeb
harizshah <harizsharizad@gmail.com>
2025-11-02 23:27:53 +0800

    create first validation

--- moviemy/src/app/genres/create-genres/create-genres.component.ts
+++ moviemy/src/app/genres/create-genres/create-genres.component.ts
@@ -1,20 +1,33 @@
 import {Component, inject} from '@angular/core';
-import {Router} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-create-genre',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, RouterLink],
   templateUrl: './create-genres.component.html',
   styleUrl: './create-genres.component.css'
 })
 export class CreateGenreComponent {
 
   router = inject(Router);
+  private formBuilder = inject(FormBuilder);
+
+  form = this.formBuilder.group({
+    name: ['', {validators: [Validators.required]}],
+  });
+
+  getErrorMessagesForName(): string {
+    let field = this.form.controls.name;
+    return "The name field is required";
+  }
 
   saveChanges(){
     // ..save changes
-
+    console.log(this.form.value);
     this.router.navigate(['/genres']);
   }
 }
