c774f45ce27c05540b4918407fdf790d84c00055
harizshah <harizsharizad@gmail.com>
2025-11-03 22:34:07 +0800

    create a map component using leaflet

--- moviemy/src/app/theaters/index-theaters/index-theaters.component.ts
+++ moviemy/src/app/theaters/index-theaters/index-theaters.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from "@angular/material/button";
-import {RouterLink} from "@angular/router";
-import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-index-theaters',
-    imports: [
-        MatIconModule,
-        MatButtonModule,
-        RouterLink
-    ],
+  imports: [MatIconModule, MatButtonModule, RouterLink],
   templateUrl: './index-theaters.component.html',
   styleUrl: './index-theaters.component.css'
 })
---
+++ moviemy/src/app/theaters/theaters.models.ts
@@ -0,0 +1,12 @@
+﻿export interface TheaterCreationDTO {
+  name: string;
+  latitude: number;
+  longitude: number;
+}
+
+export interface TheaterDTO {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+}
