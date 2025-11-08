1e665f4f60265e6766137d1e0bcff4f7eaafbce0
harizshah <harizsharizad@gmail.com>
2025-11-08 12:39:35 +0800

    prepare app to store images

--- MovieAPI/Controllers/ActorsController.cs
+++ MovieAPI/Controllers/ActorsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.OutputCaching;
 using MovieAPI.DTOs;
 using MovieAPI.Entities;
+using MovieAPI.Services;
 
 namespace MovieAPI.Controllers
 {
@@ -15,9 +16,10 @@ namespace MovieAPI.Controllers
         private readonly IMapper mapper;
         private readonly IOutputCacheStore outputCacheStore;
         private const string cacheTag = "Actors";
+        private readonly string container = "Actors";
 
         public ActorsController(ApplicationDbContext context, IMapper mapper,
-            IOutputCacheStore outputCacheStore)
+            IOutputCacheStore outputCacheStore, IFileStorage fileStorage)
         {
             this.context = context;
             this.mapper = mapper;
@@ -28,6 +30,12 @@ namespace MovieAPI.Controllers
         public async Task<IActionResult> Post([FromForm] ActorCreationDTO actorCreationDto)
         {
             var actor = mapper.Map<Actor>(actorCreationDto);
+
+            if (actorCreationDto.Picture is not null)
+            {
+                var url = await.IFileStorage.Store(container, actorCreationDto.Picture);
+                actor.Picture = url;
+            }
             
             context.Add(actor);
             await context.SaveChangesAsync();
