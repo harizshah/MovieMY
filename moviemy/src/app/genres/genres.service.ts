70286973661bba08b87283bfaf7eb37f89685ab6
harizshah <harizsharizad@gmail.com>
2025-11-09 08:50:22 +0800

    movie filter & search

--- moviemy/src/app/genres/genres.service.ts
+++ moviemy/src/app/genres/genres.service.ts
@@ -1,39 +1,44 @@
 import { inject, Injectable } from '@angular/core';
-import {GenreCreationDTO, GenreDTO} from './genres.models';
-import {HttpClient, HttpResponse} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {environment} from '../../environments/environment';
-import {PaginationDTO} from '../shared/models/PaginationDTO';
-import {buildQueryParams} from '../shared/functions/buildQueryParams';
+import { GenreCreationDTO, GenreDTO } from './genres.models';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { PaginationDTO } from '../shared/models/PaginationDTO';
+import { buildQueryParams } from '../shared/functions/buildQueryParams';
+import { ICRUDService } from '../shared/interfaces/ICRUDService';
+
 @Injectable({
   providedIn: 'root'
 })
-export class GenresService {
+export class GenresService implements ICRUDService<GenreDTO, GenreCreationDTO> {
 
   constructor() { }
 
   private http = inject(HttpClient);
-  private baseUrl = environment.apiURL + '/genres'
+  private baseURL = environment.apiURL + '/genres';
 
   public getPaginated(pagination: PaginationDTO): Observable<HttpResponse<GenreDTO[]>> {
     let queryParams = buildQueryParams(pagination);
-    return this.http.get<GenreDTO[]>(this.baseUrl, {params: queryParams, observe: 'response'});
+    return this.http.get<GenreDTO[]>(this.baseURL, {params: queryParams, observe: 'response'});
   }
 
-  public getById(id: number): Observable<GenreDTO> {
-    return this.http.get<GenreDTO>(`${this.baseUrl}/${id}`);
+  public getById(id: number): Observable<GenreDTO>{
+    return this.http.get<GenreDTO>(`${this.baseURL}/${id}`);
+  }
 
+  public getAll(): Observable<GenreDTO[]>{
+    return this.http.get<GenreDTO[]>(`${this.baseURL}/all`);
   }
 
   public create(genre: GenreCreationDTO){
-    return this.http.post(this.baseUrl, genre);
+    return this.http.post(this.baseURL, genre);
   }
 
   public update(id: number, genre: GenreCreationDTO){
-    return this.http.put(`${this.baseUrl}/${id}`, genre);
+    return this.http.put(`${this.baseURL}/${id}`, genre);
   }
 
   public delete(id: number){
-    return this.http.delete(`${this.baseUrl}/${id}`);
+    return this.http.delete(`${this.baseURL}/${id}`);
   }
 }
--- moviemy/src/app/movies/movies-search/movies-search.models.ts
+++ moviemy/src/app/movies/movies-search/movies-search.models.ts
@@ -3,4 +3,6 @@
   genreId: number;
   upcomingReleases: boolean;
   inTheaters: boolean;
+  page: number;
+  recordsPerPage: number;
 }
